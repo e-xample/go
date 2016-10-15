106c000404db799eae92199cb67cd71d552de8c6
e-xample <suganuma.koji.mail@gmail.com>
2016-10-15 21:51:56 +0900

    [add] sample.multi_cal_function

---
+++ multi_val_func.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func divMod(x, y int) (int, int) {
+    return x / y, x % y
+}
+
+/*
+func divMod(x, y int) (p, q int) {
+    p, q = x / y, x % y
+    return 
+}
+*/
+
+func main() {
+    p, q := divMod(10, 3)
+    fmt.Println(p)
+    fmt.Println(q)
+}
